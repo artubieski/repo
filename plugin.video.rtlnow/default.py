382d8f292da159a3f4dd2cce62062f06c9b9a37e
Biedulski, Artur (U718390) <artur.biedulski@lhsystems.com>
2019-07-07 20:23:39 +0200

    fixed hashplay method

--- plugin.video.rtlnow/default.py
+++ plugin.video.rtlnow/default.py
@@ -724,6 +724,8 @@ def hashplay(idd):
              if not streamcode=="0":
                stream=streamcode
                debug("STREAM : #" +stream +"#")         
+               if token=="0":
+                 ret,token=login()
           referer=deeplink        
           headerfelder="user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36" #&Referer="+referer	
 		  #headerfelder="user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36&Referer="+referer
